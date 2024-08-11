35458b11f45481e76265c028e1f974b66b3836de
Lele <wbnominat@bk.ru>
2024-08-11 15:39:07 +0300

    Add files via upload

--- module 1 3.py
+++ module 1 3.py
@@ -2,10 +2,11 @@ name = "Lilya"
 print (name, type (name))
 age= 28
 print (name, type (age))
-age = age + 2
+age=  age + 2
 print = (age)
 is_student = (type(True))
 print = is_student
+print (is_student)
 
 
 
